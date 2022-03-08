05c46851000e9a35dd42c981c2fc558ff3a5ad90
ituyama <ituyama01@outlook.jp>
2022-03-09 00:39:10 +0900

    しんきぺーじをついか

--- js/index.js
+++ js/index.js
@@ -2,3 +2,18 @@ var json = Blockly.serialization.workspaces.save(workspace);
 Blockly.serialization.workspaces.load(json);
 var workspace = Blockly.inject('blocklyDiv',                                                  // ライブラリに読み込ませる
 {toolbox: document.getElementById('toolbox')});
+
+Vue.createApp({
+    el: '#app',
+    data(){
+        return {
+            message: 'Hello Axios',
+        }
+    },
+    mounted(){
+        axios.get('https://jsoplaceholder.typicode.com/users')
+            .then(response => nconsole.log(response))
+            .catch(error => console.log(error))
+    }
+}).mount('#app')
+
